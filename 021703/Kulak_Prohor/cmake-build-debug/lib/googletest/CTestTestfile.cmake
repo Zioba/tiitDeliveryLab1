6b74beb8fe23093f854ab05c5ae65326df97484e
lagatun54 <adidak2003@gmail.com>
2021-03-13 21:06:15 +0300

    Kulak_Prohor_Lab1

---
+++ 021703/Kulak_Prohor/cmake-build-debug/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/tiit1_2-main (1)/tiit1_2-main/lib/googletest
+# Build directory: C:/tiit1_2-main (1)/tiit1_2-main/cmake-build-debug/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ 021703/Kulak_Prohor/cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/tiit1_2-main (1)/tiit1_2-main/lib/googletest/googletest
+# Build directory: C:/tiit1_2-main (1)/tiit1_2-main/cmake-build-debug/lib/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
