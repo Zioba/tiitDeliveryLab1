dc819e42b98c9547ef747059af5778d11eac1a68
Prohor Kulak <40538451+lagatun54@users.noreply.github.com>
2021-03-16 01:50:09 +0300

    Delete 021703/Kulak_Prohor directory
    delete my dir

--- 021703/Kulak_Prohor/cmake-build-debug/lib/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/tiit1_2-main (1)/tiit1_2-main/lib/googletest/googlemock
-# Build directory: C:/tiit1_2-main (1)/tiit1_2-main/cmake-build-debug/lib/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- 021703/Kulak_Prohor/cmake-build-debug/lib/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/tiit1_2-main (1)/tiit1_2-main/lib/googletest/googletest
-# Build directory: C:/tiit1_2-main (1)/tiit1_2-main/cmake-build-debug/lib/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
