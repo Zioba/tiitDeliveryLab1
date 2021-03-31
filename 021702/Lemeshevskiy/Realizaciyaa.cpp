ae74f15494a68f488a9b3013ed1bd3da5fe3c572
Jolly <lemesh12345678@gmail.com>
2021-03-31 23:20:32 +0300

    Lemeshevskiy handing in lab #1

---
+++ 021702/Lemeshevskiy/Realizaciyaa.cpp
@@ -0,0 +1,27 @@
+﻿#include "BIBLIO.h"
+
+using namespace std;
+// Driver program 
+int main()
+{
+	string str;
+	cin >> str;
+
+	vector<int>suffixArr = buildSuffixArray(str, str.length());
+	int n = suffixArr.size();
+	int i = 0, j = 0, k = 0;
+	cout << "Suffix Array : \n";
+	for (i; i<str.length(); i++)
+	{
+		cout << suffixArr[i];
+	}
+	
+	vector<int>lcp = kasai(str, suffixArr);
+
+	cout << "\nLCP Array : \n";
+	for (j; j < str.length(); j++)
+	{
+		cout << lcp[j];
+	}
+	cout <<endl <<  minLexRotation(str) << endl;
+}
