e391a34bb055b60eba7042eba1cd6fefd2c9eefd
Durnevir <Durnevir@users.noreply.github.com>
2021-05-29 12:30:37 +0300

    Lab1 by kosar Anastasia
    Lab1 by kosar Anastasia (3rd attempt)

---
+++ 021702/Kosar/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Treap1; 
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static Treap Build(int[] xs, int[] ys)
+        {
+            Console.WriteLine(xs.Length == ys.Length);
+            //System.Diagnostics.Debug.Assert(xs.Length == ys.Length);
+
+            var tree = new Treap(xs[0], ys[0]);
+            var last = tree;
+
+            for (int i = 1; i < xs.Length; ++i)
+            {
+                if (last.y > ys[i])
+                {
+                    last.Right = new Treap(xs[i], ys[i], parent: last);
+                    last = last.Right;
+                }
+                else
+                {
+                    Treap cur = last;
+                    while (cur.Parent != null && cur.y <= ys[i])
+                        cur = cur.Parent;
+                    if (cur.y <= ys[i])
+                        last = new Treap(xs[i], ys[i], cur);
+                    else
+                    {
+                        last = new Treap(xs[i], ys[i], cur.Right, null, cur);
+                        cur.Right = last;
+                    }
+                }
+            }
+
+            while (last.Parent != null)
+                last = last.Parent;
+            return last;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] xs = new int[6] { 11, 4, 29, 18, 22, 5 };
+            int[] ys = new int[6] { 67, 0, 24, 58, 64, 45 };
+            Treap result = Build(xs, ys);
+            Console.WriteLine(result.Parent);
+            Console.WriteLine(result.Left);
+            Console.WriteLine(result.Right);
+            Console.ReadKey();
+            
+
+        }
+    }
+
+}
+
---
+++ 021702/Kosar/Treap1/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treap1
+{
+        public class Treap
+        {
+            public int x;
+            public int y;
+
+            public Treap Left;
+            public Treap Right;
+            public Treap Parent;
+
+            public Treap(int x, int y, Treap left = null, Treap right = null, Treap parent = null)
+            {
+                this.x = x;
+                this.y = y;
+                this.Left = left;
+                this.Right = right;
+                this.Parent = parent;
+            }
+            public void Split(int x, out Treap L, out Treap R)
+            {
+                Treap newTree = null;
+                if (this.x <= x)
+                {
+                    if (Right == null)
+                        R = null;
+                    else
+                        Right.Split(x, out newTree, out R);
+                    L = new Treap(this.x, y, Left, newTree);
+                }
+                else
+                {
+                    if (Left == null)
+                        L = null;
+                    else
+                        Left.Split(x, out L, out newTree);
+                    R = new Treap(this.x, y, newTree, Right);
+                }
+            }
+
+            public Treap Remove(int x)
+            {
+                Treap l, m, r;
+                Split(x - 1, out l, out r);
+                r.Split(x, out m, out r);
+                return Merge(l, r);
+            }
+
+            public static Treap Merge(Treap L, Treap R)
+            {
+                if (L == null) return R;
+                if (R == null) return L;
+
+                if (L.y > R.y)
+                {
+                    var newR = Merge(L.Right, R);
+                    return new Treap(L.x, L.y, L.Left, newR);
+                }
+                else
+                {
+                    var newL = Merge(L, R.Left);
+                    return new Treap(R.x, R.y, newL, R.Right);
+                }
+            }
+
+
+        }
+
+}
